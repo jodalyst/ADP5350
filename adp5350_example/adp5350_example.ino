cadd6ff997aef3165526b2824847bdc277029cbb
Kenneth Collins <kennethc@mit.edu>
2017-10-09 16:56:10 -0400

    Added comments

--- adp5350_example/adp5350_example.ino
+++ adp5350_example/adp5350_example.ino
@@ -3,18 +3,18 @@
 
 // Set up the oled object
 const int BIT_DEPTH = 16;
-int LOOP_SPEED = 100; //milliseconds
+int LOOP_SPEED = 100; // milliseconds
 int primary_timer = 0;
 
 ADP5350 adp;
 
 void setup() {
-  Serial.begin(115200); //for debugging if needed.
+  Serial.begin(115200); // for debugging if needed.
   Wire.begin();
-  randomSeed(analogRead(0));  //initialize random numbers
+  randomSeed(analogRead(0));  // initialize random numbers
   primary_timer = millis();
 
-  adp.writeByte(ADP5350_ADDRESS,FUEL_GAUGE_MODE, 0x01);
+  adp.writeByte(ADP5350_ADDRESS,FUEL_GAUGE_MODE, 0x01); // enable Fuel Gauge
 
 }
 
